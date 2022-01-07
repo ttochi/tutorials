723f64484fce5125075ee4d849de2481f184252a
ttochi <jjiny1127@gmail.com>
2022-01-07 14:28:48 +0900

    [go] nomadcoders go for beginner theory

---
+++ go/README.md
@@ -0,0 +1,4 @@
+# GO tutorial
+
+https://go.dev/doc/tutorial/getting-started
+https://nomadcoders.co/go-for-beginners/lobby
---
+++ go/test/test.go
@@ -0,0 +1,11 @@
+package test
+
+import "fmt"
+
+func sayHi() {
+	fmt.Println("hi2")
+}
+
+func SayHi() {
+	fmt.Println("hi1")
+}
